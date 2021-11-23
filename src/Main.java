02e57c614398cfebd53f48e6b2f9adbbb96b214c
sanchesaline6 <sanches.aline6@gmail.com>
2021-11-23 07:48:59 -0300

    Mentoria, Main and Curso classes created

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("Descrição curso Java");
+        curso.setCargaHoraria(8);
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso JavaScript");
+        curso1.setDescricao("Descrição curso JavaScript");
+        curso1.setCargaHoraria(4);
+
+        System.out.println(curso);
+        System.out.println(curso1);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+    }
+}
